b114257e1170800f2ac8cf9a3318437c1fe2bc7a
Cedric Castro <cedriccastro150@gmail.com>
2026-02-08 20:41:49 +0800

    chore: fix deplotment error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Contact from "@/pages/Contact"
 import Experience from "@/pages/Experience"
 import Home from "@/pages/Home"
 import Layout from "@/components/layout/Layout"
-import Projects from "@/pages/Projects"
+// import Projects from "@/pages/Projects"
 import Skills from "@/pages/Skills"
 import { ThemeFAB } from "./components/fab"
  
